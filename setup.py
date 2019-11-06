81f453d5b1337ccd9febd0a0f6acdbd0eff792b3
leondutoit <dutoit.leon@gmail.com>
2019-11-06 20:20:08 +0100

    bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='pypg-iam',
-    version='0.1.0',
+    version='0.2.0',
     description='python library for pg-iam',
     author='Leon du Toit',
     author_email='dutoit.leon@gmail.com',
