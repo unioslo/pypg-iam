2f6ea4b34e96319b3bb75af1125e9d1c444b8bcb
Milen Kouylekov <milen.kouylekov@usit.uio.no>
2024-03-07 11:03:17 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='pypg-iam',
-    version='0.7.4',
+    version='0.7.5',
     description='python library for pg-iam',
     author='Leon du Toit, Milen Kouylekov',
     author_email='dutoit.leon@gmail.com',
